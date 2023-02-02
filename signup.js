dd35e22843cbaec451e9d3b97c5ef4af5dbe0a8f
charzlwebz256 <charzlwebz256@gmail.com>
2023-02-02 16:40:53 +0300

    Pushede

---
+++ signup.js
@@ -0,0 +1,77 @@
+// States
+var username= "";
+var firstname= "";
+var lastName ="";
+var password = "";
+var email = "";
+// Username onchange handler
+function handlerUsernameChange(inputElement){
+    username = inputElement.value;
+}
+
+// FirstName onchange handler
+function handlerFirstNameChange(inputElement){
+    firstName = inputElement.value;
+}
+
+// LastName onchange handler
+function handlerLastNameChange(inputElement){
+    lastName = inputElement.value;
+}
+
+// password onchange handler
+function handlerpasswordChange(inputElement){
+    password = inputElement.value;
+}
+
+// email onchange handler
+function handleremailChange(inputElement){
+    email = inputElement.value;
+}
+
+console.log("My document");
+function myfirstfunction(){
+    // Codes inserted here
+    console.log("This is my first function");
+}
+myfirstfunction();
+
+// Adding two numbers
+function add(a,b,c) {
+    console.log(a+b+closed);
+}
+add( 58,65,40);
+
+//  QUIZ FUNCTION
+
+// Subtraction
+function subtract(g,h) {
+    console.log(g-h);   
+}
+subtract(96,23);
+// Multiplication
+function multiply(a,b,c,d) {
+    console.log(a*b*c*d);   
+}
+multiply(9,3,6,2);
+// Division
+function divo(a,b) {
+    console.log(a/b);   
+}
+divo(150,3);
+
+// Mixed
+function mixed(a,b,c,d,e) {
+    console.log(a+b*c-d/e)
+}
+mixed(4,6,9,3,2);
+//  Arrays
+
+const countries=["Uganda", "Kenya", "Tanzania", "Rwanda", "Burundi"];
+console.log(countries[0]);
+console.log(countries[3]);
+
+function onusernamechangehandler
+(inputelement){
+    console.log(inputelement.value);
+}
